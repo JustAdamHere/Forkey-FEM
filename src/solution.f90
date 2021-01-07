5c612c22385eeb7c106301b10ecd439bdcc5d371
Adam Blakey <adam@blakey.family>
2021-01-07 13:27:51 +0000

    Added starter solution files.

---
+++ src/solution.f90
@@ -0,0 +1,17 @@
+module class_solution
+    use common
+    use class_element
+    use class_element_interval
+    use class_mesh
+
+    implicit none
+
+    public
+
+    type solution
+
+    end type
+
+contains
+    
+end module
---
+++ src/solution_cg.f90
@@ -0,0 +1,18 @@
+module class_solution_cg
+    use common
+    use class_element
+    use class_element_interval
+    use class_mesh
+    use class_solution
+
+    implicit none
+
+    public
+
+    type, extends(solution) :: solution_cg
+
+    end type
+
+contains
+
+end module
